21fa9d34d3e12716cfc795d84a8acd22b1f1ef59
Sereyvath Lynay <lynay.sereyvath1@gmail.com>
2021-05-21 20:44:29 +0700

    stack using array

--- array.cpp
+++ array.cpp
@@ -4,46 +4,70 @@ using std::cout;
 using std::cin;
 using std::endl; 
 
+#define MaxStack 100
 
-class Stack{
-    private:
-        int top;
-        int info[100];
-    public:
-        Stack(){
-            this->top = -1;
-        }
-        void push(int item){
-            if(this->top >= 100){
-                cout<<"Stack Overflows";
-            }
-            else{
-                this->top = this->top + 1;
-                this->info[this->top] = item;
-            }
-        }
-        int pop(){
-            int element;
-            element = this->info[this->top];
-            if(this->top < 0){
-                cout<<"Stack Underflows"<<endl;
-                return 0;
-            }
-            else{
-                this->top -= 1;
-                return element;
-            }
-        }
+struct Stack{
+    int info[MaxStack];
+    int top;
 };
 
+struct dyStack{
+    int info;
+    struct dyStack* top;
+}*S2;
+
+void initialize(struct dySack);
+void initialize(struct Stack* s);
+void push(struct Stack* s, int item);
+int pop(struct Stack* s);
 
 int main(){
-    Stack a1;
-    for(int i = 1; i <= 10; i++){
-        a1.push(i);
-    }
-    for(int i = 0; i < 10; i++){
-        cout<<a1.pop()<<endl;
-    }
+    struct Stack S1;
+    initialize(&S1);
+
+    for(int i = 1; i<= 10; i++)
+        push(&S1, i);
+    for(int i = 0; i< 10; i++)
+        cout<<pop(&S1)<<endl;
     return 0;
+}
+
+void initialize(struct Stack *s)
+{
+    s->top = -1;
+}
+
+void push(struct Stack *s, int item)
+{
+    if (s->top >= MaxStack)
+    {
+        cout << ("Stack overflow") << endl;
+        return;
+    }
+    s->top += 1;
+    s->info[s->top] = item;
+}
+
+int pop(struct Stack *s)
+{
+    int element = s->info[s->top];
+    if (s->top < 0)
+    {
+        cout << "Stack underflow" << endl;
+        return -1;
+    }
+    s->top -= 1;
+    return element;
+}
+
+struct dyStack* initialize(){
+    return NULL;
+}
+
+struct dyStack* getnode(){
+    return new struct dyStack;
+}
+
+struct dyStack* push(struct Dystack *s){
+    
 }
