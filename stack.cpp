74c082267152c8ed24c63821524a13ce3beecec0
Sereyvath Lynay <lynay.sereyvath1@gmail.com>
2021-05-24 14:23:02 +0700

    stack with linked list

---
+++ stack.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stack>
+#include<string.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void Reverse(char *C, int n){
+    std::stack<char> S;
+    for(int i = 0; i<n; i++)
+        S.push(C[i]);
+    for(int i = 0; i<n; i++){
+        C[i] = S.top();
+        S.pop();
+    }
+}
+
+int main(){
+    char C[51];
+    cout<<"Enter a string: "; cin>>C;
+    Reverse(C, strlen(C));
+    cout<<C;
+}
